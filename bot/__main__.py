231941e4e01c1e29580964299a03566830177ce8
C-Foxx <95162403+Cyber-Foxx@users.noreply.github.com>
2022-02-13 08:09:33 +0000

    Create __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -61,8 +61,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Report Group", "https://t.me/+MwgSi5vmQEA2N2Vk")
+    buttons.buildbutton("🅼🅰🅸🅽 🅶🆁🅾🆄🅿", "https://t.me/GraphicCommunity4")
+    buttons.buildbutton("🅲🅷🅰🅽🅽🅴🅻", "https://t.me/MrAnonymouso02")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
